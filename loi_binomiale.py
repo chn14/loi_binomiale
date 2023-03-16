d98657beb6defa859d59981f86bcf499edb2aae9
Chn <123629446+chn14@users.noreply.github.com>
2023-03-16 20:21:27 +0100

    Update loi_binomiale.py

--- loi_binomiale.py
+++ loi_binomiale.py
@@ -6,17 +6,17 @@ class Binomiale:
         self.n = n
         self.p = p
 
-    def egal(self,k):
+    def egal(self,k): #P(X=k)
         pk = math.comb(self.n,k)*(self.p**k)*((1-self.p)**(self.n-k))
         print("P(X="+str(k)+")="+str(pk))
 
-    def auplus(self,k):
+    def auplus(self,k): #P(X<=k)
         pk = 0
         for i in range(k+1):
             pk += math.comb(self.n,i)*(self.p**i)*((1-self.p)**(self.n-i))
         print("P(X<="+str(k)+")="+str(pk))
 
-    def aumoins(self,k):
+    def aumoins(self,k): #P(X>=k)
         pk = 0
         for i in range(k,self.n+1):
             pk += math.comb(self.n,i)*(self.p**i)*((1-self.p)**(self.n-i))
